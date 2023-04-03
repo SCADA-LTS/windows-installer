68ffd8c62d99a06a01e12a0d3e0f99834fd9a7f8
Kamil Jarmusik <kamil.jarmusik@gmail.com>
2023-04-03 16:38:24 +0200

    standalone version with mysql server - added init schema scadalts

--- mysql/my_init.bat
+++ mysql/my_init.bat
@@ -1,12 +1,15 @@
 @echo off
 SETLOCAL
 set DATADIR="data"
-set MY_LNG="share/english"
+set MY_LNG="english"
 set USER="root"
+set INIT_SCHEMA_PATH="%cd%\scadalts.sql"
 
 IF EXIST data rm -R data
 
 echo %DATADIR%
+echo %INIT_SCHEMA_PATH%
+
 mkdir data
 cd bin
-mysqld --datadir %DATADIR% --initialize-insecure --user=%USER% --language=%MY_LNG% --extra-sql-file=scadalts.sql
+mysqld --datadir %DATADIR% --initialize-insecure --user=%USER% --language=%MY_LNG% --init-file=%INIT_SCHEMA_PATH% --console
